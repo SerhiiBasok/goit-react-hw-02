0ae59c432174f025c55382519c6c33a73f21b850
SerhiiBasok <sergeybsk19@gmail.com>
2024-03-21 19:59:31 +0200

    upd

---
+++ goit-react-hw-02/src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import Options from "../Options/Options.jsx";
+import Feedback from "../Feedback/Feedback.jsx";
+import Descriptions from "../Description/Descriptions.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Descriptions />
+      <Options />
+      <Feedback />
+    </>
+  );
+};
+
+export default App;
---
+++ goit-react-hw-02/src/components/Options/Options.jsx
@@ -0,0 +1,12 @@
+const Options = ({ option }) => {
+  return (
+    <div>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <button>Reset</button>
+    </div>
+  );
+};
+
+export default Options;
